136f31f8f47f0b863388aa15be8973071cf9d060
masquam <masquam@yahoo.co.jp>
2019-02-27 16:40:56 +0900

    sanitize

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var router = express.Router();
 var flash = require("connect-flash");
 var passport = require('passport')
+var sanitize = require('mongo-sanitize');
 
 var csrfProtection = csrf({ cookie: true })
 var parseForm = bodyParser.urlencoded({ extended: false })
@@ -11,7 +12,9 @@ var parseForm = bodyParser.urlencoded({ extended: false })
 
 
 router.get('/', csrfProtection, function(req, res, next) {
-  res.render('login', {username: req.user, message: req.flash("error"), csrfToken: req.csrfToken() });
+  res.render('login', 
+    {username: sanitize(req.user), message: req.flash("error"),
+     csrfToken: req.csrfToken() });
 });
 router.post('/', parseForm, csrfProtection,
   passport.authenticate('local', { successRedirect: '/admin/menu.html',
